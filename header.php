b715fdf931cacccb9757ecca59408c8a82a9c408
nod <116609221+2101404@users.noreply.github.com>
2022-10-28 09:57:38 +0900

    includeした際のいらない部分を削除

--- header.php
+++ header.php
@@ -1,51 +1,35 @@
-  <!DOCTYPE html>
-  <html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
-  <link rel="stylesheet" href="./css/style.css">
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+  <div class="container-fluid">
+    <!-- タイトル -->
+    <a class="navbar-brand" href="index.php">
+      <img src="./imgs/logo.png" height="46px" alt="">
+      AKAYAMA
+    </a>
 
-    <title>menu</title>
-  </head>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
+      <!-- 検索 -->
+      <form class="d-flex" action="./item_search_result.php">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
 
-  <body>
-      <nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
-        <div class="container-fluid">
-          <!-- タイトル -->
-          <a class="navbar-brand" href="index.php">
-            <img src="./imgs/logo.png" height="46px" alt="">
-            AKAYAMA
-          </a>
+      <!-- 右側のボタン類 -->
+      <ul class="navbar-nav  mb-2 mb-md-0">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="./mypage.php"><i class="bi bi-person-circle"></i>マイページ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./cart.php"><i class="bi bi-cart"></i>買い物カゴ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./login.php"><i class="bi bi-door-open"></i>ログイン</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-            <!-- 検索 -->
-            <form class="d-flex" action="./item_search_result.php">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
 
-            <!-- 右側のボタン類 -->
-            <ul class="navbar-nav  mb-2 mb-md-0">
-              <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="./mypage.php"><i class="bi bi-person-circle"></i>マイページ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./cart.php"><i class="bi bi-cart"></i>買い物カゴ</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="./login.php"><i class="bi bi-door-open"></i>ログイン</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-  </body>
-  </html>
-  

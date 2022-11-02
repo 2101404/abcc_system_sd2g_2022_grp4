48acec51e1e57d1a62102cf81c03f7c70adc6393
2101466 <116697744+2101466@users.noreply.github.com>
2022-11-02 12:06:11 +0900

    追加した

--- item_search_result.php
+++ item_search_result.php
@@ -15,6 +15,37 @@
     <?php include "header.php" ?>
 
     <h2>商品検索結果</h2>
+    <form action="index.php" method="POST">
+    検索キーワード:<input type="text" name="key"><br>
+    検索一致件数:<input type="text" name="ken"><br>
+    </form>
+
+    <!-- セレクトボックスの読み込み -->
+    <?php require '../index.php';?>
+    <p>並び順</p>
+    <from action="index.php" method="POST">
+        <select name="seat">
+            <option value="最新順">最新順</option>
+            <option value="サイズ順">サイズ順</option>
+    </select>
+    <p><input type="submit" value="確定"></p>
+    </from>
+    <?php require '../item_search_result.php';?>
+
+    <!-- 商品の読み込み -->
+    <div class=" p-3 frame">
+			<div class="row">
+			<?php
+					$itemImg =['./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png','./imgs/sample/noimage.png',];
+					$text =['商品1','商品2','商品3','商品4','商品5','商品6','商品7','商品8','商品9','商品10','商品11','商品12','商品13','商品14','商品15','商品16','商品17','商品18','商品19','商品20'];
+					$itemLink =['100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000','100000',];
+					showCategory($itemLink,$itemImg,$text);
+				?>
+
+    <!-- ボタン -->
+	<div class="text-center mt-5 mb-5">
+		<button type="button" class="btn btn-lg btn-outline-primary">トップページに戻る</button>
+	</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

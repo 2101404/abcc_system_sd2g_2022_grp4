9ddd887b48b60afc47ddcdf0210b75c449e0e6e2
hayabusa1225 <116698114+hayabusa1225@users.noreply.github.com>
2022-11-04 11:22:42 +0900

    登録データベース追加

--- DBManager.php
+++ DBManager.php
@@ -15,5 +15,20 @@
             return $ps->fetchAll();
         }
 
+        public function insertmember(
+            $mail,$pass,$sei,$mei,$hurigana_sei,$hurigana_mei,$seibetsu,$birth,$phone_num,$jusho)
+            {
+                $pdo = $this->dbConnect();
+                $sql = "INSERT INTO member(mail,pass,sei,mei,hurigana_sei,hurigana_mei,seibetsu,birth,phone_num,jusho)
+                VALUES(?,?,?,?,?,?,?,?,?,?)";
+                $ps = $pdo->prepare($sql);
+                $ps->bindValue(1,$mail);
+                $ps->bindValue(2,$pass);         $ps->bindValue(3,$sei);
+                $ps->bindValue(4,$mei);          $ps->bindValue(5,$hurigana_sei);
+                $ps->bindValue(6,$hurigana_mei); $ps->bindValue(7,$seibetsu);
+                $ps->bindValue(8,$birth);        $ps->bindValue(9,$phone_num);
+                $ps->bindValue(10,$jusho);
+                $ps->execute();
+        }
     }
 ?>

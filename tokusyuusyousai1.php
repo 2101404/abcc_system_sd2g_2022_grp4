fb51531fb63347452bdf3f51a7cc186337ee3585
0040302 <2101379@s.asojuku.ac.jp>
2022-11-09 12:04:35 +0900

    tokusyuugamensakusei

---
+++ tokusyuusyousai1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>特集詳細画面１</title>
+</head>
+<body>
+    <!-- ヘッダーの読み込み -->
+    <?php include "header.php" ?>
+
+<div class ="container">
+
+    <img class="img-fluid rounded" src="./imgs/sample/tokusyuu1.png" alt="tokusyuu1">
+    <p>ネクタイのキレイな結び方を動画でわかりやすくご紹介。</p>
+    
+    <!-- 1 -->
+    <div class="container alert-dark text-center">
+    <p>-ポイント1-</p>
+    <h4>プレーンノット</h4>
+    <p>プレーンノットは最も簡単で基本的な結び方で、初めてネクタイをする方におすすめです。<br>
+    一般的なレギュラーカラーのシャツとの相性も良く、ビジネスシーンはもちろん就活や結婚式、お葬式など、<br>
+    あらゆるシーンで活用できます。<br>
+    きつく締めすぎると、結び目部分が小さくなってしまうので注意が必要です。</p>
+    </div>
+
+    <img class="img-fluid rounded" src="./imgs/sample/ts1-1.jpg" alt="nekutai">
+
+    <!-- 2 -->
+    <div class="container alert-dark text-center">
+    <p>-ポイント2-</p>
+    <h4>肩幅</h4>
+    <p>肩幅まわりに<br>
+    重みが均等だと着心地が軽い</p>
+    </div>
+
+    <img class="img-fluid rounded" src="./imgs/sample/ts1-2.jpg" alt="kata">
+
+    <h4>肩</h4>
+    <p>ジャケットの肩山と肩のトップ位置がちょうど<br>
+    合っている状態で、ジャケットの肩をつまんで<br>
+    1㎝程度のゆとりがあるのが適正サイズです。<br>
+    それ以上つまめると大きく、ジャケットの肩が<br>
+    落ちてしまうのでだらしのない印象になります。<br>
+    つまめる余裕がない場合は小さく、動きにくくなります。</p>
+
+    <!-- movie -->
+
+    <!-- b -->
+    <button class = "btn btn-light btn-lg" >トップページに戻る</button>
+    
+</div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ tokusyuusyousai3.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css">
+
+    <title>特集詳細画面３</title>
+</head>
+<body>
+    <!-- ヘッダーの読み込み -->
+    <?php include "header.php" ?>
+
+<div class ="container">
+
+    <img class="img-fluid rounded" src="./imgs/sample/tokusyuu3.jpg" alt="tokusyuu3">
+    <p>汗や汚れに強く、しわになりにくいスーツのお手入れ方法をご紹介。<br>
+    上手にメンテナンスして長くキレイに使用しましょう。</p>
+
+    <!-- 1 -->
+    <div class="container alert-dark text-center">
+    <p>-ポイント1-</p>
+    <h4>汗を落とす手順</h4>
+    <p>汗をきれいに落とす手順を分かりやすくご紹介します</p>
+    </div>
+
+        <p>ぬるま湯につけたタオルを硬く絞ります。<br>
+        汗のつきやすい箇所を上からトントンと軽く叩きます。<br>
+        〇部分が特に汚れやすい箇所です。汚れがひどい時には、<br>
+        ぬるま湯に中性洗剤を溶かし、標準使用量の洗剤液を作ります。<br>
+        1・2と同じ手順で汚れ部分を叩きます。</p>
+
+        <p>汗のつきやすい箇所を上からトントンと軽く叩きます</p>
+
+        <p>〇部分が特に汚れやすい箇所です。<br>
+        汚れがひどい時には、ぬるま湯に中性洗剤を<br>
+        溶かし、標準使用量の洗剤液を作ります。<br>
+        1・2と同じ手順で汚れ部分を叩きます。</p>
+
+        <p>洗剤成分を取るために、ぬるま湯を2回程<br>
+        とりかえながら、内側に乾いたタオルを入れ、<br>
+        絞ったタオルで叩き拭きします。</p>
+
+        <p>風通しの良い日陰にハンガー掛けし、湿気を飛ばします。</p>
+
+    <!-- 2 -->
+    <div class="container alert-dark text-center">
+    <p>-ポイント2-</p>
+    <h4>シミや汚れを落とす手順</h4>
+    <p>シミや汚れがスーツについてしまった時の<br>
+    落とし方の手順を分かりやすくご紹介します</p>
+    </div>
+
+        <p>①タオルの下に、板や雑誌を敷き、<br>
+        シミの部分を下にします。</p>
+
+        <p>②綿棒の液をたれない程度にたっぷり<br>
+        しみ込ませ、シミのまわりから叩きはじめます。</p>
+
+        <p>③シミのまわりから内側に向かって、綿棒を<br>
+        垂直にして上からトントンと叩くと輪ジミの防止になります。</p>
+
+        <p>④場所を変えて何度かくり返し、タオルにシミを移し取ります。</p>
+
+    <!-- movie -->
+
+    <!-- b -->
+    <button class = "btn btn-light btn-lg" >トップページに戻る</button>
+
+</div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>

b0fadd56c9750dc5d1831c3eac55352b8ac42c09
hayabusa1225 <116698114+hayabusa1225@users.noreply.github.com>
2022-11-11 12:23:43 +0900

    dbm修正

--- DBManager.php
+++ DBManager.php
@@ -14,7 +14,8 @@
             
             return $ps->fetchAll();
         }
-
+        
+        //新規登録画面
         public function insertmember(
             $mail,$pass,$sei,$mei,$hurigana_sei,$hurigana_mei,$seibetsu,$birth,$phone_num,$jusho)
             {
@@ -22,12 +23,13 @@
                 $sql = "INSERT INTO member(mail,pass,sei,mei,hurigana_sei,hurigana_mei,seibetsu,birth,phone_num,jusho)
                 VALUES(?,?,?,?,?,?,?,?,?,?)";
                 $ps = $pdo->prepare($sql);
-                $ps->bindValue(1,$mail,PDO::PARAM_STR);         $ps->bindValue(2,$pass,PDO::PARAM_STR);
+                $ps->bindValue(1,$mail,PDO::PARAM_STR);         $ps->bindValue(2,password_hash($_POST["pass"],PASSWORD_DEFAULT),PDO::PARAM_STR);
                 $ps->bindValue(3,$sei,PDO::PARAM_STR);          $ps->bindValue(4,$mei,PDO::PARAM_STR);
                 $ps->bindValue(5,$hurigana_sei,PDO::PARAM_STR); $ps->bindValue(6,$hurigana_mei,PDO::PARAM_STR);
                 $ps->bindValue(7,$seibetsu,PDO::PARAM_STR);     $ps->bindValue(8,$birth,PDO::PARAM_STR);
                 $ps->bindValue(9,$phone_num,PDO::PARAM_STR);    $ps->bindValue(10,$jusho,PDO::PARAM_STR);
                 $ps->execute();
+                
         // 会員IDを使って、注文表・注文詳細表・商品表からデータをとってくる
         public function getBuyHistory($memberId){
             $pdo = $this->dbConnect();

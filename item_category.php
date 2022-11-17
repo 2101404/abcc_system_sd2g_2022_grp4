f0772c5d1533ba9e48c76f7a1b5a890d55b500ae
nod <116609221+2101404@users.noreply.github.com>
2022-11-17 19:30:02 +0900

    商品カテゴリー　サイドバーに被らないように

--- item_category.php
+++ item_category.php
@@ -16,32 +16,32 @@
   <!-- サイドバーの読み込み -->
   <?php include "side.php" ?>
 
-  <h2>カテゴリー別商品</h2>
-  <h3>
-    <?php 
-      $category = $_GET['category'];
-      echo "カテゴリー名:$category";
-    ?>
-  </h3>
+  <div id="container"class="container-fluid">
+    <h2 class="mt-5 mb-3">カテゴリー別商品</h2>
+    <h3 class="mt-2 mb-3">
+      <?php 
+        $category = $_GET['category'];
+        echo "カテゴリー名:$category";
+      ?>
+    </h3>
 
 
-  <!-- セレクトボックスの読み込み -->
-  <label class="col-sm-4 control-label">並び替え</label>
-  <div class="text-center mt-5 mb-5">
-  <select class="form-select" name="num">
-    <option selected>並び替え</option>
-    <option value="1">最新順</option>
-    <option value="2">安い順</option>
-    <option value="2">高い順</option>
-  </select>
+    <!-- セレクトボックスの読み込み -->
+    <label for="sortSelect" class="col-sm-4 control-label">並び替え</label>
+    <select id="sortSelect" class="form-select mt-1 mb-5" name="num">
+      <option selected>並び替え</option>
+      <option value="1">最新順</option>
+      <option value="2">安い順</option>
+      <option value="2">高い順</option>
+    </select>
+
+    <a href="./item_detail.php">商品詳細</a>
+    <!-- 戻るボタン -->
+    <div class="text-center mt-5 mb-5">
+      <a type="button" href="./index.php?" class="btn btn-lg btn-outline-primary mt-5">トップページに戻る</a>
+    </div>
+
   </div>
-
-  <a href="./item_detail.php">商品詳細</a>
-  <!-- 戻るボタン -->
-  <div class="text-center mt-5 mb-5">
-    <a type="button" href="./index.php?" class="btn btn-lg btn-outline-primary mt-5">トップページに戻る</a>
-  </div>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>

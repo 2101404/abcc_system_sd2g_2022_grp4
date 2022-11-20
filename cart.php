7db182465b223f5f07b22d63f97275858c67ddf1
かえる先生 <116697833+yoshi185@users.noreply.github.com>
2022-11-21 05:31:04 +0900

    Update cart.php

--- cart.php
+++ cart.php
@@ -10,11 +10,13 @@
 
     <title>cart</title>
 </head>
+<<<<<<< HEAD
 
     <?php
         function showHistory($date,$image,$itemName,$suryo,$size,$price){
             echo'
                 <!-- 商品のリスト -->
+                <div name="co"
                 <div class="col-12">
                     <div class="card">
                         <div class="row g-0">
@@ -63,10 +65,12 @@
         }
     ?>
 
+=======
+>>>>>>> 814c1cc991c768536ff37aa4d837985d747bdcb0
     <title>買い物かご</title>
 </head>
 <body>
-    <h2>買い物かご</h2>
+    
     <a href="./cart_delete.php">削除</a><br>
     <a href="./order_confirmation.php">注文</a>
     <?php require_once "DBManager.php"; ?>
@@ -82,43 +86,41 @@
             $memberId = 1;
 
             $dbm = new DBManager();
-            $tbl = $dbm->getBuyHistory($memberId);
-            echo "<div class=\"row gy-1\">";
+            $tbl = $dbm->getCart($memberId);
             
             $sum = 0;
-            $orderId = 0;
-            $cnt = 0;
-            foreach($tbl as $row){
-                $rowcnt = count($tbl);
-                // 初回の処理
-                if($cnt == 0){
-                    $orderId = $row['order_id'];
-                    showDate($row['order_date']);
-                }
-
-                if($orderId == $row['order_id'] || $cnt == 0){
-                    // 注文番号が同じ商品の金額を合計する
-                    $sum += $row['item_price'];
-                }else{
-                    // 注文番号の区切り 合計金額の表示と注文日の表示
-                    showSum($sum);
-                    showDate($row['order_date']);
-                    $sum = 0;
-                    $sum += $row['item_price'];
-                }
-
-                // 商品の履歴を表示
-                showHistory($row['order_date'],$row['item_image'],$row['item_name'],$row['suryo_data'],$row['item_size'],$row['item_price']);
-
-                $orderId = $row['order_id'];
-                $cnt++;
-                // 最後の商品を表示した後の処理
-                if($cnt == $rowcnt){
-                    showSum($sum);
-                }
-            }
-            echo "</div> ";
         ?>
+        <div class="row gy-2">
+            <?php foreach($tbl as $row): ?>
+                <!-- 商品のリスト -->
+                <div class="col-12">
+                    <div class="card">
+                        <div class="row g-0">
+
+                            <div class="col-4 col-md-3">
+                                <div class="ratio ratio-1x1">
+                                    <img src="<?php echo $row['item_image']?>"   alt="..." >
+                                </div>
+                            </div>
+                            
+                            <div class="col-8 col-md-9">
+                                <div class="card-body">
+                                    <h3 class="card-title"><?= $row['item_name'] ?></h5>
+                                    <p class="card-text fs-4">
+                                            <span class="me-3">数量：<?= $row['cart_suryo']?></span>
+                                            <span>サイズ：<?= $row['cart_size']?></span>
+                                            <span style="float:right;"><?= number_format($row['item_price'])?>円</span>
+                                    </p>
+                                    <a href="" style="float:right;">削除</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach;?>
+        </div>
+        
         
         <div class="text-right my-3">    
             <a type="button" class="btn btn-outline-primary" href="">注文</a>
@@ -129,7 +131,20 @@
         </div>
                      
     </div>
-    
+    <?php
+        function showSum($sum){
+            echo' <!-- 合計金額 -->
+                    <div class="row">
+                        <div class="col-12">
+                            <p class="text-end my-3 fs-3"> 合計 '.$sum.'円</p>
+                        </div>
+                    </div>    
+                ';
+
+
+        }
+    ?>
+
 
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
--- insertSample.sql
+++ insertSample.sql
@@ -1,12 +1,31 @@
 -- 会員テーブル
 INSERT INTO member(mail,pass,sei,mei,hurigana_sei,hurigana_mei,seibetsu,birth,phone_num,jusho)
 VALUES('abc@example.com',12345,'山田','太郎','ヤマダ','タロウ','男','2022-1-1','08012345678','福岡県福岡市');
+INSERT INTO member(mail,pass,sei,mei,hurigana_sei,hurigana_mei,seibetsu,birth,phone_num,jusho)
+VALUES('def@example.com',67890,'山田','花子','ヤマダ','ハナコ','女','2022-10-3','09012345678','佐賀県佐賀市');
 
 -- カテゴリーテーブル
-INSERT INTO `category` (`category_id`, `category_name`) VALUES (NULL, 'スーツ');
+INSERT INTO category(category_name) VALUES('スーツ');
 INSERT INTO category(category_name) VALUES('バッグ');
+INSERT INTO category(category_name) VALUES('ベルト');
+INSERT INTO category(category_name) VALUES('コート');
+INSERT INTO category(category_name) VALUES('ジャケット');
+INSERT INTO category(category_name) VALUES('パンツ');
+INSERT INTO category(category_name) VALUES('セットアップ');
+INSERT INTO category(category_name) VALUES('靴');
+INSERT INTO category(category_name) VALUES('スカート');
+INSERT INTO category(category_name) VALUES('靴下');
+INSERT INTO category(category_name) VALUES('ネクタイ');
 
 -- 商品テーブル
-INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, is_sale, item_sale_price, item_registration_date)
-VALUES(1,'./imgs/category/suit01.jpg','スーツテスト','商品説明です','黒',10000,FALSE,8000,'2022-11-1');
+INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, item_size, is_sale, item_sale_price, item_registration_date)
+VALUES((SELECT category_id FROM category WHERE category_name = 'スーツ'),'./imgs/category/suit01.jpg','スーツテスト','商品説明です','黒',10000,'S,M,L,XL',FALSE,8000,'2021-11-1');
+INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, item_size, is_sale, item_sale_price, item_registration_date)
+VALUES((SELECT category_id FROM category WHERE category_name = 'バッグ'),'./imgs/category/bag01.jpg','バッグ','バッグです','黒',10000,'S', FALSE,8000,'2022-10-1');
+INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, item_size, is_sale, item_sale_price, item_registration_date)
+VALUES((SELECT category_id FROM category WHERE category_name = 'ベルト'),'./imgs/category/belt03.png','ベルト','ベルトです','茶',3000,'XS,S,M,L', FALSE,2500,'2022-11-1');
+INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, item_size, is_sale, item_sale_price, item_registration_date)
+VALUES((SELECT category_id FROM category WHERE category_name = '靴'),'./imgs/category/shoes01.jpg','靴','靴です','黒',5000,'S,M,L', FALSE,2500,CURRENT_DATE());
+INSERT INTO item(category_id, item_image, item_name, item_description, item_color, item_price, item_size, is_sale, item_sale_price, item_registration_date)
+VALUES((SELECT category_id FROM category WHERE category_name = 'スーツ'),'./imgs/sample/ikeoji.png','イケオジになっちゃうスタイリッシュスーツ！！','大人の雰囲気を醸し出す、スタイリッシュなスーツです。 これを着ればあなたもきっとイケオジに。','黒',750000,'S,M,L',FALSE,600000,CURRENT_DATE());
 

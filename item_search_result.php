583af40ef8dd97d325e4a3ff42cc5208e004a005
nod <116609221+2101404@users.noreply.github.com>
2022-11-27 08:44:07 +0900

    調整

--- item_search_result.php
+++ item_search_result.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
   <link rel="stylesheet" href="./css/style.css">
   <link rel="stylesheet" href="./css/style1.css">
@@ -36,12 +36,12 @@
     
     
     <!-- カード -->
-    <div class="row">
+    <div class="row gy-2">
       <!-- ループで回して商品情報を表示 -->
       <?php foreach($itemTbl as $row):?>
         <div class="col-6 col-md-3">
 
-          <div class="card h-100 p-2">
+          <div class="card p-2" style="height:360px">
             
             <div class="image-area h-50">
               <a href="./item_detail.php?itemId=<?=$row['item_id']?>">
@@ -52,7 +52,7 @@
             <div class ="content">
               <div class="card-wrap">
                 <div class ="card-list">
-                  <h2 class = "card-Title "><?=$row['item_name']?></h2>
+                  <h3 class = "card-Title "><?=$row['item_name']?></h3>
                   <p class ="card-listText text-center">￥<?=number_format($row['sellingPrice'])?>(税込)</p>
                 </div>
               </div>
@@ -89,6 +89,6 @@
       btn.click();//ボタンクリックしたときと同じ動作
     }
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 </html>
